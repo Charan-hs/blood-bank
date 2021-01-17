f676f283e28aa39c73e5c1ac15473fa410d6c82c
Charan <charanengg08@gmail.com>
2021-01-17 11:07:42 +0530

    BLOOD BANK

--- app.js
+++ app.js
@@ -857,4 +857,4 @@ app.get('',(req,res) => {
   res.redirect('/')
 })
 
-app.listen(3000, () => console.log("server started on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("server started "));
